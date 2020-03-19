183c86eb4e97ee7388c44630e096b070548540b6
Jesus Torres <jesu_ti91@hotmail.com>
2020-03-19 11:15:19 -0500

    Update IndexController.java

--- spring-boot-web/src/main/java/co/com/jdti/springbootweb/app/controllers/IndexController.java
+++ spring-boot-web/src/main/java/co/com/jdti/springbootweb/app/controllers/IndexController.java
@@ -4,7 +4,11 @@ import co.com.jdti.springbootweb.app.models.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequestMapping("/app")
@@ -26,5 +30,22 @@ public class IndexController {
         model.addAttribute("title", "Welcome!!");
         model.addAttribute("subTitle", "Welcome " + user.getName());
         return "profile";
+    }
+
+    @GetMapping("/list")
+    public String toList(Model model) {
+        model.addAttribute("title", "List users");
+        return "list";
+    }
+
+    @ModelAttribute("users")
+    public List<User> populateList() {
+        List<User> users = new ArrayList<>();
+        users.add(new User("Fulano", "De tal", "fulano@mail.com"));
+        users.add(new User("Sutano", "Perez", "sutano@mail.com"));
+        users.add(new User("Mengano", "Patolo", "mengano@mail.com"));
+        users.add(new User("Tornado", "Roe", "mengano@mail.com"));
+        users.add(new User("John", "Doe", "mengano@mail.com"));
+        return users;
     }
 }

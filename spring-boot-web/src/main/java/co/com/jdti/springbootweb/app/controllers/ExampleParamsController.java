931d6c803d6b2fc3aaf78a9c041a1a73d15b61b0
Jesus David Torres <jesu_ti91@hotmail.com>
2020-03-19 14:55:32 -0500

    Parametro de tipo HttpServletRequest

--- spring-boot-web/src/main/java/co/com/jdti/springbootweb/app/controllers/ExampleParamsController.java
+++ spring-boot-web/src/main/java/co/com/jdti/springbootweb/app/controllers/ExampleParamsController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 @RequestMapping("/param")
 public class ExampleParamsController {
@@ -24,6 +26,14 @@ public class ExampleParamsController {
     @GetMapping("/mix-param")
     public String param(@RequestParam(required = false, defaultValue = "empty") String text, @RequestParam int num, Model model) {
         model.addAttribute("result", "The text sent is: '" + text + "' and the number is: '" + num + "'");
+        return "params/see";
+    }
+
+    @GetMapping("/mix-param-request")
+    public String param(HttpServletRequest request, Model model) {
+        String text = request.getParameter("text");
+        int number = Integer.parseInt(request.getParameter("num"));
+        model.addAttribute("result", "The text sent is: '" + text + "' and the number is: '" + number + "' con HttpServlet");
         return "params/see";
     }
 }

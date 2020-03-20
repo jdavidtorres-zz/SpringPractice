c17bdec7d91ea31bb7bd1f1c25d56c914040c2de
Jesus David Torres <jesu_ti91@hotmail.com>
2020-03-20 15:54:06 -0500

    Texto desde properties con la notación Value

--- spring-boot-web/src/main/java/co/com/jdti/springbootweb/app/controllers/IndexController.java
+++ spring-boot-web/src/main/java/co/com/jdti/springbootweb/app/controllers/IndexController.java
@@ -1,6 +1,7 @@
 package co.com.jdti.springbootweb.app.controllers;
 
 import co.com.jdti.springbootweb.app.models.User;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,9 +15,20 @@ import java.util.List;
 @RequestMapping("/app")
 public class IndexController {
 
+    private final String titleConstant = "title";
+
+    @Value("${text.indexcontroller.index.title}")
+    private String indexText;
+
+    @Value("${text.indexcontroller.profile.title}")
+    private String profileText;
+
+    @Value("${text.indexcontroller.list.title}")
+    private String listText;
+
     @GetMapping({"/index", "/", "", "/home"})
     public String index(Model model) {
-        model.addAttribute("title", "Hello World");
+        model.addAttribute(titleConstant, indexText);
         return "index";
     }
 
@@ -27,14 +39,13 @@ public class IndexController {
         user.setSurname("de tal");
         user.setEmail("fulano@mail.com");
         model.addAttribute("user", user);
-        model.addAttribute("title", "Welcome!!");
-        model.addAttribute("subTitle", "Welcome " + user.getName());
+        model.addAttribute(titleConstant, "Welcome!!");
         return "profile";
     }
 
     @GetMapping("/list")
     public String toList(Model model) {
-        model.addAttribute("title", "List users");
+        model.addAttribute(titleConstant, listText);
         return "list";
     }
 
@@ -44,8 +55,8 @@ public class IndexController {
         users.add(new User("Fulano", "De tal", "fulano@mail.com"));
         users.add(new User("Sutano", "Perez", "sutano@mail.com"));
         users.add(new User("Mengano", "Patolo", "mengano@mail.com"));
-        users.add(new User("Tornado", "Roe", "mengano@mail.com"));
-        users.add(new User("John", "Doe", "mengano@mail.com"));
+        users.add(new User("Tornado", "Roe", "tornado@mail.com"));
+        users.add(new User("John", "Doe", "doe@mail.com"));
         return users;
     }
 }

346f27e6c6e421385a3cf966372a738aba483f0a
Jesus David Torres <jesu_ti91@hotmail.com>
2020-03-30 10:14:10 -0500

    Avance en el curso

---
+++ spring-boot-form/src/main/java/co/com/jdti/springbootform/app/controllers/FormController.java
@@ -0,0 +1,31 @@
+package co.com.jdti.springbootform.app.controllers;
+
+import co.com.jdti.springbootform.app.models.domain.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
+
+@Controller
+public class FormController {
+
+    @GetMapping("/form")
+    public String form(Model model) {
+        model.addAttribute("title", "Form");
+        return "form";
+    }
+
+    @PostMapping("/form")
+    public String sendForm(@Valid User user, BindingResult result, Model model) {
+        if (result.hasErrors()) {
+
+            return "form";
+        }
+        model.addAttribute("title", "Result!!");
+        model.addAttribute("user", user);
+        return "result";
+    }
+}

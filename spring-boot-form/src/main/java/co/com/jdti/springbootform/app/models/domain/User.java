8d63e9ba9a85a487d6e219615887f2400432e86b
Jesus David Torres <jesu_ti91@hotmail.com>
2020-04-19 17:47:31 -0500

    Nuevos campos

--- spring-boot-form/src/main/java/co/com/jdti/springbootform/app/models/domain/User.java
+++ spring-boot-form/src/main/java/co/com/jdti/springbootform/app/models/domain/User.java
@@ -5,6 +5,12 @@ import javax.validation.constraints.NotEmpty;
 public class User {
 
     @NotEmpty(message = "Can´t be empty")
+    private String name;
+
+    @NotEmpty(message = "Can´t be empty")
+    private String lastName;
+
+    @NotEmpty(message = "Can´t be empty")
     private String username;
 
     @NotEmpty(message = "Can´t be empty")
@@ -16,7 +22,9 @@ public class User {
     public User() {
     }
 
-    public User(String username, String password, String email) {
+    public User(String name, String lastName, String username, String password, String email) {
+        this.name = name;
+        this.lastName = lastName;
         this.username = username;
         this.password = password;
         this.email = email;
@@ -44,5 +52,21 @@ public class User {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
     }
 }
